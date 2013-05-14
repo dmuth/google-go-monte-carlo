b02c44fa7369442c5ce2ae73bd8e69bae024c584
Douglas Muth <dmuth@dmuth.org>
2013-05-13 22:24:47 -0400

    Now requiring chunk size to be a multiple of 2.

--- src/args/args.go
+++ src/args/args.go
@@ -5,6 +5,7 @@
 package args;
 
 import "flag"
+import "fmt"
 import "os"
 
 
@@ -16,6 +17,7 @@ type Config struct {
 	Num_points int
 	Num_goroutines int
 	Random_md5 bool
+	Chunk_size uint64
 }
 
 
@@ -31,9 +33,15 @@ func ParseArgs() (Config) {
 	flag.IntVar(&config.Num_points, "num-points", 10, "How many points to plot?")
 	flag.IntVar(&config.Num_goroutines, "num-goroutines", 10, "How many goroutines to use for generating random numbers")
 	flag.BoolVar(&config.Random_md5, "random-md5", false, "Set to use MD5 for faux random number generation")
+	flag.Uint64Var(&config.Chunk_size, "chunk-size", 10000, "Create random numbers in batches of this size (must be a multiple of 2)")
 	help := flag.Bool("help", false, "test2")
 	h := flag.Bool("h", false, "To get this help")
 	flag.Parse()
+
+	if (config.Chunk_size % 2 != 0) {
+		fmt.Println("chunk-size must be a multiple of 2!")
+		os.Exit(1)
+	}
 
 	if (*help || *h) {
 		flag.PrintDefaults()
