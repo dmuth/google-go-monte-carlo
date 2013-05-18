04f989b683d8b3649ae52b7663321a88c3abc3ea
Douglas Muth <dmuth@dmuth.org>
2013-05-18 00:21:58 -0400

    Formatting.

--- src/args/args.go
+++ src/args/args.go
@@ -31,13 +31,19 @@ func ParseArgs() (Config) {
 
 	config := Config{}
 
-	flag.Uint64Var(&config.Size, "size", 10, "How big to make the grid for the circle quadrant")
-	flag.IntVar(&config.Num_points, "num-points", 10, "How many points to plot?")
-	flag.IntVar(&config.Num_goroutines, "num-goroutines", 10, "How many goroutines to use for generating random numbers")
-	flag.BoolVar(&config.Random_md5, "random-md5", false, "Set to use MD5 for faux random number generation")
-	flag.Uint64Var(&config.Chunk_size, "chunk-size", 10000, "Create random numbers in batches of this size (must be a multiple of 2)")
-	debug_level := flag.String("debug-level", "info", "Set our logging level. Can be: error, warn, info, debug, or trace")
-	help := flag.Bool("help", false, "test2")
+	flag.Uint64Var(&config.Size, "size", 10, 
+		"How big to make the grid for the circle quadrant")
+	flag.IntVar(&config.Num_points, "num-points", 10, 
+		"How many points to plot?")
+	flag.IntVar(&config.Num_goroutines, "num-goroutines", 10, 
+		"How many goroutines to use for generating random numbers")
+	flag.BoolVar(&config.Random_md5, "random-md5", false, 
+		"Set to use MD5 for faux random number generation")
+	flag.Uint64Var(&config.Chunk_size, "chunk-size", 10000, 
+		"Create random numbers in batches of this size (must be a multiple of 2)")
+	debug_level := flag.String("debug-level", "info", 
+		"Set our logging level. Can be: error, warn, info, debug, or trace")
+	help := flag.Bool("help", false, "To get this help")
 	h := flag.Bool("h", false, "To get this help")
 	flag.Parse()
 
